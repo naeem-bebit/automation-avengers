b28b9bb14152f34e8b0e226c88ed7364c4a4b0f0
Naeem Hussien <naeemhussien@gmail.com>
2024-08-07 22:35:35 +0800

    auto trigger

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def hello_world():
 
 @app.route('/newpage')
 def new_page():
-    return 'Welcome to the new page!'
+    return 'Welcome to the new page! Testing Jenkins auto trigger'
 
 if __name__ == '__main__':
     app.run()
