af00a440ab1cbe2c8def0e50bc8aed2043e8578b
Abi Dzar <mail.dzar@gmail.com>
2024-08-15 00:06:12 +0800

    Change text in landing page

--- tests/test_app.py
+++ tests/test_app.py
@@ -15,7 +15,7 @@ class AppTestCase(unittest.TestCase):
     def test_hello_world(self):
         result = self.app.get('/')
         self.assertEqual(result.status_code, 200)
-        self.assertIn(b'Hello, World!', result.data)
+        self.assertIn(b'Hello,', result.data)
         self.assertIn(b'Check KL Azan Time', result.data)
 
     def test_klazan_page(self):
